822359a217d7aa35080fe9094445b253a08db741
Audipunk <audipunkwp@gmail.com>
2024-09-13 11:33:31 +0200

    Update Final

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// # [JSL04] Submission: Monster Ternary Operator
-
-// #### Challenge 1: Flavor Selection
-
-
-
-let userPreference = 'Sugar-free';
+let userPreference = "Sugar-free";
 function checkPreferred() {
-  return userPreference === "Regular" ?
-  `${userPreference}` :
-  userPreference === "Sugar-free" ?
-  `${userPreference}` :
-  "Unknown-flavor";
+  return userPreference === "Regular"
+    ? `${userPreference}`
+    : userPreference === "Sugar-free"
+    ? `${userPreference}`
+    : "Unknown-flavor";
 }
-
 console.log(`User prefers: ${checkPreferred()}`);
 
 // #### Challenge 2: Stock Check
@@ -21,31 +14,25 @@ let cansLeft = 3; // Any number of cans
 const canStock = cansLeft <= 5 ? "Time to restock!" : "We're stocked!";
 console.log(`${canStock}`);
 
-
 // #### Challenge 3: Workout Intensity
-
-
 
 let heartRate = 99; // Current heart rate in bpm
 const bpm = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
 console.log(`${bpm}`);
 
-
 // #### Challenge 4: Temperature Suitability
 
-
-
 let currentTemp = 4; // Current temperature in °C
-const TempCheck = currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!";
+const TempCheck =
+  currentTemp <= 5 ? `Chilled to perfection!` : `Needs a cooler!`;
 console.log(TempCheck);
 
-
-
 // #### Challenge 5: Late Night Coding Session
-let currentHour = currentDate.getHours();
 const currentDate = new Date();
-
+let currentHour = currentDate.getHours();
 
 const monsterTime =
-currentHour >= 7 && currentHour <= 23 ? "Unleash the beast!" : "Better stick to water.";
+  currentHour >= 7 && currentHour <= 23
+    ? "Unleash the beast!"
+    : "Better stick to water.";
 console.log(monsterTime);
